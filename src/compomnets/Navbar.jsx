3ec3f69b5b431b6bee2f26841368695051d23d55
naitik <naitik2408>
2025-02-24 01:11:26 +0530

    update cart icons

--- src/compomnets/Navbar.jsx
+++ src/compomnets/Navbar.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { BsHandbag } from "react-icons/bs";
 import { GiChickenOven } from "react-icons/gi";
 import { Link } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
 import Cart from './Cart/Cart';
 
 function Navbar() {
@@ -27,8 +28,14 @@ function Navbar() {
     };
 
     const totalAmount = addedDishes.reduce((total, dish) => total + dish.price * dish.quantity, 0);
+    const totalItems = addedDishes.reduce((total, dish) => total + dish.quantity, 0);
 
     const handleConfirmOrder = () => {
+        if (!address) {
+            toast.error('Delivery address is required');
+            return;
+        }
+
         const orderDetails = addedDishes.map((dish, index) => {
             return `${index + 1}️⃣ ${dish.name} - ${dish.quantity} ${dish.quantity > 1 ? 'Pieces' : 'Piece'}`;
         }).join('\n');
@@ -50,10 +57,13 @@ function Navbar() {
             <div className='hidden md:block'></div>
             <div className='flex justify-end items-center'>
                 <div
-                    className='p-2 md:p-3 shadow-lg shadow-red-200 border border-red-100 rounded-full cursor-pointer hover:bg-red-400 hover:text-white transition-all duration-300 ease-in-out'
+                    className='p-2 md:p-3 shadow-lg shadow-red-200 border border-red-100 rounded-full cursor-pointer hover:bg-red-400 hover:text-white transition-all duration-300 ease-in-out relative group'
                     onClick={handleOpenModal}
                 >
                     <BsHandbag size={20} />
+                    <div className='absolute w-7 poppins-regular right-8 aspect-square flex justify-center items-center bg-red-400 rounded-full text-white bottom-6 text-sm group-hover:bg-stone-50 group-hover:text-red-500 group-hover:border group-hover:border-stone-300 transition-all duration-300'>
+                        {totalItems}
+                    </div>
                 </div>
             </div>
 
@@ -65,15 +75,15 @@ function Navbar() {
                             <div className='text-lg font-semibold'>Total</div>
                             <div className='text-lg font-semibold'>₹{totalAmount.toFixed(2)}</div>
                         </div>
-                        <div className='mt-4'>
-                            <label htmlFor="address" className='block text-sm font-medium text-gray-700'>Delivery Address</label>
+                        <div className='mt-8'>
+                            <label htmlFor="address" className='block font-medium text-gray-700 poppins-regular'>Delivery Address</label>
                             <input 
                                 type="text" 
                                 id="address" 
                                 name="address" 
                                 value={address} 
                                 onChange={(e) => setAddress(e.target.value)} 
-                                className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm sm:text-sm outline-stone-600 outline-1'
+                                className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm outline-stone-600 outline-1'
                                 placeholder="Enter your delivery address"
                             />
                         </div>
